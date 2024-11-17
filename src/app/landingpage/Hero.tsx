58b94a1000d09744c09d990f2f5a182cdd9501d0
Yugalkishor1 <yugalyadav755@gmail.com>
2024-11-17 07:37:38 +0530

    initial setup

--- src/app/landingpage/Hero.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hero () {
-    return(
-        <div>
-            Hero jjjkl
-        </div>
-    )
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,14 @@
+import {DM_Sans} from "next/font/google"
 import "./globals.css";
+import { Metadata } from "next";
+
+const dmsaans = DM_Sans({subsets:["latin"],weight:"400"})
+
+export const metadata:Metadata = {
+  title:"automationhub",
+  description:"workflow automation",
+  keywords:"automation"
+}
 
 export default function RootLayout({
   children,
@@ -6,11 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body
+    <html lang="en">
+      <body className={`${dmsaans.className} antialiased bg-[#EAEEFE]`}
       >
         {children}
       </body>
     </html>
   );
 }
+
+
+
+
+
+
